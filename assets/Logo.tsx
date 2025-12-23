dae6914d6733d02aeb53b84225a7f3eabe9933db
m0ztop <mert.oeztopkara@nu3.de>
2025-12-24 00:45:42 +0100

    Fix logo import for production build

--- assets/Logo.tsx
+++ assets/Logo.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
+import logoImage from './logo.png';
 
 /**
  * Logo Asset
  * 
- * This component renders the "/logo.png" file.
- * Ensure you have placed your image file named "logo.png" in the public folder.
+ * This component renders the logo image.
  */
 export const Logo: React.FC = () => {
   return (
     <img 
-      src="/logo.png" 
+      src={logoImage} 
       alt="DoneKit Logo" 
       className="h-10 w-auto rounded-[13px] object-contain" 
     />
